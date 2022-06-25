5d62bd6c0168d5c77c7c44aa022ed8724caa5e52
Jasper Drescher <jasperdre@gmail.com>
2022-06-25 22:41:32 +0200

    Update Rectangle.h

--- Source/Framework/Math/Rectangle.h
+++ Source/Framework/Math/Rectangle.h
@@ -3,13 +3,11 @@
 class Rectangle
 {
 public:
-    int x;
-    int y;
-    int w;
-    int h;
+    float myX;
+    float myY;
+    float myWidth;
+    float myHeight;
 
-    Rectangle() : x(0), y(0), w(0), h(0) {}
-    Rectangle(int nx, int ny, int nw, int nh) { x = nx; y = ny; w = nw; h = nh; }
-
-    void Set(int nx, int ny, int nw, int nh) { x = nx; y = ny; w = nw; h = nh; }
+    Rectangle() : myX(0.0f), myY(0.0f), myWidth(0.0f), myHeight(0.0f) {}
+    Rectangle(float nx, float ny, float nw, float nh) { myX = nx; myY = ny; myWidth = nw; myHeight = nh; }
 };
